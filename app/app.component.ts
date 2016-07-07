79475ae0fcaf07cb85a4d1b420416169e5c3dba8
Artem Zinoviev <arzinoviev@gmail.com>
2016-07-07 16:14:40 +0300

    simulate slow connection

--- app/app.component.ts
+++ app/app.component.ts
@@ -82,7 +82,7 @@ export class AppComponent implements OnInit{
   constructor(private heroService: HeroService) {}
 
   getHeroes() {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero) {
