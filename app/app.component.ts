6ea1e905e92d0ddf04a63e9aadac1dd725295b89
Artem Zinoviev <arzinoviev@gmail.com>
2016-07-08 14:13:20 +0300

    add dashboard and basic routes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
-import {HeroesComponent} from "./heroes.component";
-import {HeroService} from "./hero.service";
+import { ROUTER_DIRECTIVES } from '@angular/router';
+import { HeroService }     from './hero.service';
 
 @Component({
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <my-heroes></my-heroes>
+    <nav>
+      <a [routerLink]="['/dashboard']" routerLinkActive="active">Dashboard</a>
+      <a [routerLink]="['/heroes']" routerLinkActive="active">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
   `,
-  directives: [HeroesComponent],
-  providers: [HeroService]
+  directives: [ROUTER_DIRECTIVES],
+  providers: [
+    HeroService
+  ]
 })
-
 export class AppComponent {
   title = 'Tour of Heroes';
 }
